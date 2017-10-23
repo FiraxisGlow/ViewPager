542b66eab51424528a364b7e485c567a40246550
Anton Kiri <K3268@Labranet.jamk.fi>
2017-10-23 09:35:04 +0300

    Initial commit

---
+++ app/src/main/java/fi/jamk/viewpager/AboutFragment.java
@@ -0,0 +1,22 @@
+package fi.jamk.viewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by K3268 on 23-Oct-17.
+ */
+public class AboutFragment  extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText("About Fragment Content");
+        return rootView;
+    }
+}
+
---
+++ app/src/main/java/fi/jamk/viewpager/MainFragment.java
@@ -0,0 +1,23 @@
+package fi.jamk.viewpager;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by K3268 on 23-Oct-17.
+ */
+
+public class MainFragment  extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText("Main Fragment Content");
+            return rootView;
+        }
+    }
